7beb1051e064a0c4c6d7fc5662268203caf025c8
eofs <tomi@madlab.fi>
2013-09-03 11:29:46 +0300

    Test models

--- oauth_api/tests/__init__.py
+++ oauth_api/tests/__init__.py
@@ -1,1 +1,2 @@
 from oauth_api.tests.test_resource_owner import *
+from oauth_api.tests.test_models import *
---
+++ oauth_api/tests/test_models.py
@@ -0,0 +1,71 @@
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from oauth_api.models import AccessToken, get_application_model
+
+
+Appliation = get_application_model()
+User = get_user_model()
+
+
+class TestModels(TestCase):
+    def setUp(self):
+        self.dev_user = User.objects.create_user('dev_user', 'dev_user@example.com', '1234')
+
+    def test_allow_scopes(self):
+        app = Appliation(
+            name='Test App',
+            redirect_uris='http://localhost http://example.com',
+            user=self.dev_user,
+            client_type=Appliation.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Appliation.GRANT_AUTHORIZATION_CODE,
+        )
+
+        access_token = AccessToken(
+            user=self.dev_user,
+            scope='read write',
+            expires=0,
+            token='',
+            application=app,
+        )
+
+        self.assertTrue(access_token.allow_scopes(['read', 'write']))
+        self.assertTrue(access_token.allow_scopes(['write', 'read']))
+        self.assertTrue(access_token.allow_scopes(['write', 'read', 'read']))
+        self.assertTrue(access_token.allow_scopes([]))
+        self.assertFalse(access_token.allow_scopes(['read', 'invalid']))
+
+    def test_grant_allinone(self):
+        app = Appliation(
+            name='Test App',
+            redirect_uris='',
+            user=self.dev_user,
+            client_type=Appliation.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Appliation.GRANT_ALLINONE,
+        )
+
+        self.assertRaises(ValidationError, app.full_clean)
+
+    def test_grant_authorization_code(self):
+        app = Appliation(
+            name='Test App',
+            redirect_uris='',
+            user=self.dev_user,
+            client_type=Appliation.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Appliation.GRANT_AUTHORIZATION_CODE,
+        )
+
+        self.assertRaises(ValidationError, app.full_clean)
+
+    def test_grant_implicit(self):
+        app = Appliation(
+            name='Test App',
+            redirect_uris='',
+            user=self.dev_user,
+            client_type=Appliation.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Appliation.GRANT_IMPLICIT,
+        )
+
+        self.assertRaises(ValidationError, app.full_clean)
+
