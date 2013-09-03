8e5a15a99508d71c8a7e5d87e939de9412969b68
eofs <tomi@madlab.fi>
2013-09-03 14:45:41 +0300

    Test if access token is valid
    Also do not override request format per request.

--- oauth_api/tests/test_client_credentials.py
+++ oauth_api/tests/test_client_credentials.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIRequestFactory
 
 from oauth_api.models import get_application_model
 from oauth_api.settings import oauth_api_settings
@@ -26,6 +26,8 @@ class BaseTest(APITestCase):
         )
         self.application.save()
 
+        self.request_factory = APIRequestFactory()
+
     def get_basic_auth(self, username, password):
         payload = '%s:%s' % (username, password)
         auth = base64.b64encode(payload.encode('utf-8')).decode('utf-8')
@@ -40,13 +42,16 @@ class BaseTest(APITestCase):
 
 class TestClientCredentials(BaseTest):
     def test_basic_auth(self):
+        """
+        Authenticate using Basic Authentication
+        """
         self.client.credentials(HTTP_AUTHORIZATION=self.get_basic_auth(self.application.client_id,
                                                                        self.application.client_secret))
         url = reverse('oauth_api:token')
         data = {
             'grant_type': 'client_credentials',
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(url, data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['token_type'], 'Bearer')
@@ -63,9 +68,75 @@ class TestClientCredentials(BaseTest):
             'client_id': self.application.client_id,
             'client_secret': self.application.client_secret,
         }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(url, data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['token_type'], 'Bearer')
         self.assertTrue(self.scopes_valid(response.data['scope'], oauth_api_settings.SCOPES))
         self.assertEqual(response.data['expires_in'], oauth_api_settings.ACCESS_TOKEN_EXPIRATION)
+
+    def test_access(self):
+        """
+        Request an access toke and try to fetch data using it
+        """
+        self.client.credentials(HTTP_AUTHORIZATION=self.get_basic_auth(self.application.client_id,
+                                                                       self.application.client_secret))
+        url = reverse('oauth_api:token')
+        data = {
+            'grant_type': 'client_credentials',
+        }
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        access_token = response.data['access_token']
+
+        # Update Basic Auth information
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer %s' % access_token)
+
+        url = reverse('resource-view')
+        response = self.client.get(url)
+
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'hello': 'world!'})
+
+    def test_denied_access(self):
+        """
+        Request an access toke and try to fetch data using it
+        """
+        self.client.credentials(HTTP_AUTHORIZATION=self.get_basic_auth(self.application.client_id,
+                                                                       self.application.client_secret))
+        url = reverse('oauth_api:token')
+        data = {
+            'grant_type': 'client_credentials',
+            'scope': 'read',
+        }
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        access_token = response.data['access_token']
+
+        # Update Basic Auth information
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer %s' % access_token)
+
+        url = reverse('resource-view')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_invalid_scope(self):
+        """
+        Request an access toke and try to fetch data using it
+        """
+        self.client.credentials(HTTP_AUTHORIZATION=self.get_basic_auth(self.application.client_id,
+                                                                       self.application.client_secret))
+        url = reverse('oauth_api:token')
+        data = {
+            'grant_type': 'client_credentials',
+            'scope': 'invalid',
+        }
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
