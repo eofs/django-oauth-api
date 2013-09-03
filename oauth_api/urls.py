42714a0f4400133a90aa631aaa2754ccd407f4b4
eofs <tomi@madlab.fi>
2013-09-03 15:22:28 +0300

    "/authorize" URL renamed to be more consistent with the actual path

--- oauth_api/urls.py
+++ oauth_api/urls.py
@@ -4,6 +4,6 @@ from oauth_api.views import AuthorizationView, TokenView
 
 
 urlpatterns = patterns('',
-    url(r'^authorize/$', AuthorizationView.as_view(), name='authorization'),
+    url(r'^authorize/$', AuthorizationView.as_view(), name='authorize'),
     url(r'^token/$', TokenView.as_view(), name='token'),
 )
