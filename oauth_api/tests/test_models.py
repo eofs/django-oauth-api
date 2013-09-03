204e90529413d37f8691d288b3f1ddd98a64002e
eofs <tomi@madlab.fi>
2013-09-03 11:33:43 +0300

    Validate model functions

--- oauth_api/tests/test_models.py
+++ oauth_api/tests/test_models.py
@@ -36,6 +36,39 @@ class TestModels(TestCase):
         self.assertTrue(access_token.allow_scopes([]))
         self.assertFalse(access_token.allow_scopes(['read', 'invalid']))
 
+    def test_default_redirect_uri(self):
+        app = Appliation(
+            name='Test App',
+            redirect_uris='http://localhost http://example.com',
+            user=self.dev_user,
+            client_type=Appliation.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Appliation.GRANT_ALLINONE,
+        )
+
+        self.assertEqual(app.default_redirect_uri, 'http://localhost')
+
+    def test_valid_redirect_uri(self):
+        app = Appliation(
+            name='Test App',
+            redirect_uris='http://localhost http://example.com',
+            user=self.dev_user,
+            client_type=Appliation.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Appliation.GRANT_ALLINONE,
+        )
+
+        self.assertTrue(app.redirect_uri_allowed('http://localhost'))
+
+    def test_invalid_redirect_uri(self):
+        app = Appliation(
+            name='Test App',
+            redirect_uris='http://localhost http://example.com',
+            user=self.dev_user,
+            client_type=Appliation.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Appliation.GRANT_ALLINONE,
+        )
+
+        self.assertFalse(app.redirect_uri_allowed('http://invalid.local.host'))
+
     def test_grant_allinone(self):
         app = Appliation(
             name='Test App',
