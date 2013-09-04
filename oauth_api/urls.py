0c6a71cd0ba8d4e3a4fdd4c24a60781b3d1e379f
eofs <tomi@madlab.fi>
2013-09-04 11:56:34 +0300

    Require login when accessing /authorize endpoint

--- oauth_api/urls.py
+++ oauth_api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from oauth_api.views import AuthorizationView, TokenView
 
 
 urlpatterns = patterns('',
-    url(r'^authorize/$', AuthorizationView.as_view(), name='authorize'),
+    url(r'^authorize/$', login_required(AuthorizationView.as_view()), name='authorize'),
     url(r'^token/$', TokenView.as_view(), name='token'),
 )
