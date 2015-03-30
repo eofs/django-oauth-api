4aa317f4561a796909b837ed7431207ba3e875d3
eofs <tomi@madlab.fi>
2015-03-30 10:31:49 +0300

    Use Django 1.7 migrations, South support dropped.

--- oauth_api/migrations/0001_initial.py
+++ oauth_api/migrations/0001_initial.py
@@ -1,181 +1,95 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import oauth_api.generators
+import oauth_api.utils
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Application'
-        db.create_table(u'oauth_api_application', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('client_id', self.gf('django.db.models.fields.CharField')(default=u'7JR+HE4g5/&=n+2?olLF_YhnU7/eNx`a&T,oNh_5R|,>Y*n;\\U\\31m6AZ,n bo|S', unique=True, max_length=100)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['customuser.CustomUser'])),
-            ('redirect_uris', self.gf('django.db.models.fields.TextField')(blank=True)),
-            ('client_type', self.gf('django.db.models.fields.CharField')(max_length=32)),
-            ('authorization_grant_type', self.gf('django.db.models.fields.CharField')(max_length=32)),
-            ('client_secret', self.gf('django.db.models.fields.CharField')(default=u"PU]5;'QJv17fy93i-cjrkqm_b@'{ve4&wB{Rmg8\\iOmM(n70H_,U*ByM-W`8^'kUP@'@^0z7-j{|CG\\B4M,j+Sy>Iu@;7(hBghrcK]yB2ofAm_g9SAogzS^%h9_qK3A)", max_length=255, blank=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-        ))
-        db.send_create_signal(u'oauth_api', ['Application'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-        # Adding model 'AccessToken'
-        db.create_table(u'oauth_api_accesstoken', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['customuser.CustomUser'])),
-            ('token', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('application', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['oauth_api.Application'])),
-            ('expires', self.gf('django.db.models.fields.DateTimeField')()),
-            ('scope', self.gf('django.db.models.fields.TextField')(blank=True)),
-        ))
-        db.send_create_signal(u'oauth_api', ['AccessToken'])
-
-        # Adding model 'AuthorizationCode'
-        db.create_table(u'oauth_api_authorizationcode', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['customuser.CustomUser'])),
-            ('code', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('application', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['oauth_api.Application'])),
-            ('expires', self.gf('django.db.models.fields.DateTimeField')()),
-            ('redirect_uri', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('scope', self.gf('django.db.models.fields.TextField')(blank=True)),
-        ))
-        db.send_create_signal(u'oauth_api', ['AuthorizationCode'])
-
-        # Adding model 'RefreshToken'
-        db.create_table(u'oauth_api_refreshtoken', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['customuser.CustomUser'])),
-            ('token', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('application', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['oauth_api.Application'])),
-            ('access_token', self.gf('django.db.models.fields.related.OneToOneField')(related_name='refresh_token', unique=True, to=orm['oauth_api.AccessToken'])),
-        ))
-        db.send_create_signal(u'oauth_api', ['RefreshToken'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Application'
-        db.delete_table(u'oauth_api_application')
-
-        # Deleting model 'AccessToken'
-        db.delete_table(u'oauth_api_accesstoken')
-
-        # Deleting model 'AuthorizationCode'
-        db.delete_table(u'oauth_api_authorizationcode')
-
-        # Deleting model 'RefreshToken'
-        db.delete_table(u'oauth_api_refreshtoken')
-
-
-    models = {
-        u'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        u'auth.permission': {
-            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        u'customuser.customuser': {
-            'Meta': {'object_name': 'CustomUser'},
-            'activities_read': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'null': 'True', 'blank': 'True'}),
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '254', 'db_index': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_login_from': ('django.db.models.fields.IPAddressField', [], {'default': "'0.0.0.0'", 'max_length': '15'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'registered_from': ('django.db.models.fields.IPAddressField', [], {'default': "'0.0.0.0'", 'max_length': '15'}),
-            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '15'})
-        },
-        u'metric.metricobject': {
-            'Meta': {'unique_together': "(('content_type', 'object_id', 'key'),)", 'object_name': 'MetricObject'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'key': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
-            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'value': ('django.db.models.fields.DecimalField', [], {'max_digits': '11', 'decimal_places': '4'})
-        },
-        u'oauth_api.accesstoken': {
-            'Meta': {'object_name': 'AccessToken'},
-            'application': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['oauth_api.Application']"}),
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'expires': ('django.db.models.fields.DateTimeField', [], {}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'scope': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'token': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['customuser.CustomUser']"})
-        },
-        u'oauth_api.application': {
-            'Meta': {'object_name': 'Application'},
-            'authorization_grant_type': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
-            'client_id': ('django.db.models.fields.CharField', [], {'default': 'u"Uj>Cp2\'.y>vh+PM1^\\\\TnG+rYuzNV_ABMH%U KK3Dc?_q\\\\V;or/Bt?9ey$x1!(cQ|"', 'unique': 'True', 'max_length': '100'}),
-            'client_secret': ('django.db.models.fields.CharField', [], {'default': 'u\'xe-? bw*k\\\'o9oHS>/SBtKQi)h83L\\\\\\\'f+^Pq}F}9H]pQ&]EF3[FKiOxS:DE!!^Y))Wsb"86]w5fAeYU?\\\\{.P5qbjn@Y#,c\\\\\\\'u6tXr)N_d1ua]opRfuUeRyq|Ir?7\\\'bBBD\'', 'max_length': '255', 'blank': 'True'}),
-            'client_type': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'redirect_uris': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['customuser.CustomUser']"})
-        },
-        u'oauth_api.authorizationcode': {
-            'Meta': {'object_name': 'AuthorizationCode'},
-            'application': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['oauth_api.Application']"}),
-            'code': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'expires': ('django.db.models.fields.DateTimeField', [], {}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'redirect_uri': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'scope': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['customuser.CustomUser']"})
-        },
-        u'oauth_api.refreshtoken': {
-            'Meta': {'object_name': 'RefreshToken'},
-            'access_token': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'refresh_token'", 'unique': 'True', 'to': u"orm['oauth_api.AccessToken']"}),
-            'application': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['oauth_api.Application']"}),
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'token': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['customuser.CustomUser']"})
-        }
-    }
-
-    complete_apps = ['oauth_api']
+    operations = [
+        migrations.CreateModel(
+            name='AccessToken',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name=b'created')),
+                ('updated', models.DateTimeField(auto_now=True, verbose_name=b'updated')),
+                ('token', models.CharField(max_length=255, db_index=True)),
+                ('expires', models.DateTimeField()),
+                ('scope', models.TextField(blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Application',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name=b'created')),
+                ('updated', models.DateTimeField(auto_now=True, verbose_name=b'updated')),
+                ('client_id', models.CharField(default=oauth_api.generators.generate_client_id, unique=True, max_length=100)),
+                ('redirect_uris', models.TextField(help_text='Allowed URIs list, line separated', blank=True, validators=[oauth_api.utils.validate_uris])),
+                ('client_type', models.CharField(max_length=32, choices=[(b'confidential', 'Confidential'), (b'public', 'Public')])),
+                ('authorization_grant_type', models.CharField(max_length=32, choices=[(b'authorization-code', 'Authorization code'), (b'implicit', 'Implicit'), (b'password', 'Resource owner password-based'), (b'client-credentials', 'Client credentials')])),
+                ('client_secret', models.CharField(default=oauth_api.generators.generate_client_secret, max_length=255, blank=True)),
+                ('name', models.CharField(max_length=255, blank=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='AuthorizationCode',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name=b'created')),
+                ('updated', models.DateTimeField(auto_now=True, verbose_name=b'updated')),
+                ('code', models.CharField(max_length=255)),
+                ('expires', models.DateTimeField()),
+                ('redirect_uri', models.CharField(max_length=255)),
+                ('scope', models.TextField(blank=True)),
+                ('application', models.ForeignKey(to='oauth_api.Application')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='RefreshToken',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name=b'created')),
+                ('updated', models.DateTimeField(auto_now=True, verbose_name=b'updated')),
+                ('token', models.CharField(max_length=255)),
+                ('expires', models.DateTimeField(null=True, blank=True)),
+                ('access_token', models.OneToOneField(related_name='refresh_token', to='oauth_api.AccessToken')),
+                ('application', models.ForeignKey(to='oauth_api.Application')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='accesstoken',
+            name='application',
+            field=models.ForeignKey(to='oauth_api.Application'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='accesstoken',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
+        ),
+    ]
