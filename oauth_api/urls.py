95bf9b500dab326553a5a8a17d5c6da1a34f6ac4
Niko Salakka <nsalakka@gmail.com>
2018-08-20 08:56:56 +0300

    Added app_name to urls for better Python 3 compatibility

--- oauth_api/urls.py
+++ oauth_api/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 from oauth_api.views import AuthorizationView, TokenView, TokenRevocationView
 
+app_name = 'oauth_api'
 urlpatterns = [
     url(r'^authorize/$', login_required(AuthorizationView.as_view()), name='authorize'),
     url(r'^token/$', TokenView.as_view(), name='token'),
