61d9b0b0906d7c37dd9487e5a562d1735642de4b
eofs <tomi@madlab.fi>
2022-03-22 11:46:08 +0200

    Incorrect identifier

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Framework :: Django",
-        "Framework :: Django :: 3.3",
+        "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
